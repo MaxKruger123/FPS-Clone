b3e788f85cc72b1b72444ab63ac7ad9d50838fab
MaxKrugerrr <157470714+MaxKrugerrr@users.noreply.github.com>
2024-10-05 01:05:17 +0200

    Added reload system and particles

--- FPS Clone/Assets/Script/GunScript.cs
+++ FPS Clone/Assets/Script/GunScript.cs
@@ -7,32 +7,91 @@ public class GunScript : MonoBehaviour
     public float damage = 10f;
     public float range = 100f;
     public ParticleSystem muzzleFlash;
+    public ParticleSystem impact;
     public Camera fpsCam;
+    public int maxAmmo = 10;
+    private int currentAmmo;
+    public float reloadTime = 1f;
+    private bool isReloading = false;
+    public Animator animator;
+
+
+    private void Start()
+    {
+        currentAmmo = maxAmmo;
+
+        Animator animator = GetComponent<Animator>();
+    }
+
+    void OnEnable()
+    {
+        isReloading = false;
+        animator.SetBool("isReloading", false);
+    }
 
     // Update is called once per frame
     void Update()
     {
+        if (isReloading)
+        {
+            return;
+        }
+        if (currentAmmo <= 0)
+        {
+            StartCoroutine(Reload());
+            return;
+        }
         if (Input.GetButtonDown("Fire1"))
         {
             Shoot();
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            StartCoroutine(Reload());
+            return;
         }
     }
 
     void Shoot()
     {
-        muzzleFlash.Play();
-       RaycastHit hit;
-        if (Physics.Raycast(transform.position, transform.forward, out hit, range))
-        {
-            Debug.Log(hit.transform.name);
-            Target target = hit.transform.GetComponent<Target>();
+        // Decrease ammo
+        currentAmmo--;
 
-            if (target != null )
+        // Play muzzle flash effect
+        muzzleFlash.Play();
+
+        // Create a RaycastHit variable to store information about the object hit by the ray
+        RaycastHit hit;
+
+        // Cast a ray from the camera's position, in the direction the camera is looking
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+        {
+            Debug.Log(hit.transform.name);  // Log the name of the object hit
+
+            // Check if the object hit has a 'Target' component and apply damage if so
+            Target target = hit.transform.GetComponent<Target>();
+            if (target != null)
             {
-                target.TakeDamage(5f);
+                target.TakeDamage(damage);  // Apply damage to the target
             }
-            
-            
+
+            // Instantiate the impact effect at the hit point, facing the hit surface's normal
+            ParticleSystem impactGO = Instantiate(impact, hit.point, Quaternion.LookRotation(hit.normal));
+
+            // Destroy the impact effect after 2 seconds
+            Destroy(impactGO.gameObject, 2f);
         }
+    }
+
+    IEnumerator Reload()
+    {
+        isReloading = true;
+        animator.SetBool("isReloading", true);
+        Debug.Log("Reloading...");
+        yield return new WaitForSeconds(reloadTime);
+        currentAmmo = maxAmmo;
+        isReloading = false;
+        animator.SetBool("isReloading", false);
     }
 }
